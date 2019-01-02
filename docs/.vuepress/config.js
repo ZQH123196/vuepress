da40e2daba05f209e3697b5d239e29ba564c3cce
zqh123196 <zqh123196@gmail.com>
2019-01-02 18:30:17 +0800

    updata

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   title: 'Hello !',
   description: '郑启华的技术文档',
+  plugins: ['@vuepress/back-to-top'],
   themeConfig: {
     nav: [{
         text: '文档',
@@ -14,10 +15,14 @@ module.exports = {
     sidebar: {
       '/': [
         ['/', '首页'],
-        ['/blog/one', 'one'],
+        '/blog/one',
         ['/blog/two', 'text']
-      ],
+      ]
     }
+  },
+  markdown: {
+    lineNumbers: true
   },
   lastUpdated: 'Last Updated', // string | boolean
 }
+// VuePress 内置了基于 headers 的搜索 —— 它会自动为所有页面的标题、h2 和 h3 构建起一个简单的搜索索引。
