9796ca87bbef4a0d69d2638b8222b593b96ac0f5
zqh123196 <zqh123196@gmail.com>
2019-01-10 23:33:20 +0800

    updata

--- docs/.vuepress/components/TurnImagesWalls.vue
+++ docs/.vuepress/components/TurnImagesWalls.vue
@@ -1,81 +1,24 @@
 <template>
-  <div class="wrapper" :class="{ wrapperActive: wrapperActive }">
+  <div
+    class="wrapper"
+    :class="{ wrapperActive: wrapperActive }"
+  >
     <ul>
       <li
         class="item"
-        @click="handleClickActive"
+        @click.stop="handleClickActive"
+        v-for="i of number"
+        :key="i"
       >
         <div :class="init">
           <div class="image"></div>
           <div class="title">image</div>
           <div class="dir">
             <div class="header">bird</div>
-            <div class="close"></div>
-          </div>
-        </div>
-      </li>
-      <li
-        class="item"
-        @click="handleClickActive"
-      >
-        <div :class="init">
-          <div class="image"></div>
-          <div class="title">image</div>
-          <div class="dir">
-            <div class="header">bird</div>
-            <div class="close"></div>
-          </div>
-        </div>
-      </li>
-      <li
-        class="item"
-        @click="handleClickActive"
-      >
-        <div :class="init">
-          <div class="image"></div>
-          <div class="title">image</div>
-          <div class="dir">
-            <div class="header">bird</div>
-            <div class="close"></div>
-          </div>
-        </div>
-      </li>
-      <li
-        class="item"
-        @click="handleClickActive"
-      >
-        <div :class="init">
-          <div class="image"></div>
-          <div class="title">image</div>
-          <div class="dir">
-            <div class="header">bird</div>
-            <div class="close"></div>
-          </div>
-        </div>
-      </li>
-      <li
-        class="item"
-        @click="handleClickActive"
-      >
-        <div :class="init">
-          <div class="image"></div>
-          <div class="title">image</div>
-          <div class="dir">
-            <div class="header">bird</div>
-            <div class="close"></div>
-          </div>
-        </div>
-      </li>
-      <li
-        class="item"
-        @click="handleClickActive"
-      >
-        <div :class="init">
-          <div class="image"></div>
-          <div class="title">image</div>
-          <div class="dir">
-            <div class="header">bird</div>
-            <div class="close"></div>
+            <div
+              class="close"
+              @click.stop="handleClickClose"
+            ></div>
           </div>
         </div>
       </li>
@@ -89,20 +32,29 @@ export default {
     return {
       init: "init inner",
       wrapperActive: false,
+      number: 6
     };
   },
-  computed: {
-
-  },
+  computed: {},
   methods: {
     initFunc() {
       // 删除 init 以达到动画效果
       this.init = "inner";
     },
     handleClickActive(e) {
-      let liElement = e.srcElement.parentElement.parentElement
-      liElement.classList.add('active');
-      this.wrapperActive = true
+      console.log("add");
+
+      let liElement = e.srcElement.parentElement.parentElement;
+      liElement.classList.add("active");
+      this.$refs.active = liElement;
+      this.wrapperActive = true;
+    },
+    handleClickClose(e) {
+      this.wrapperActive = false;
+      let liEl = this.$refs.active;
+      liEl.classList.remove("active");
+      console.log(this.$refs);
+      console.log("remove");
     }
   },
   mounted() {
@@ -111,58 +63,11 @@ export default {
 };
 </script>
 
-<style >
+<style lang="stylus" scoped>
 * {
   margin: 0;
   padding: 0;
   list-style: none;
-}
-
-.wrapper {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 40rem;
-  width: 60rem;
-  background-color: black;
-  border: 1px solid red;
-}
-
-li {
-  width: 100%;
-  height: 100%;
-}
-.wrapper .inner {
-  width: 100%;
-  height: 100%;
-  cursor: pointer;
-  transition: transform 1s linear;
-}
-
-.wrapper .init {
-  transform: translateY(100%);
-}
-
-/* 点击放大 */
-.wrapperActive ul li {
-  transition: height 1s linear, width 1s linear;
-}
-
-.wrapperActive ul li.active {
-  width: 100%
-}
-.wrapperActive ul li:not(.active) {
-  height: 0;
-  width: 0;
-}
-
-.wrapper ul li .image {
-  width: 100%;
-  height: 100%;
-  border-radius: 20px;
-  background-size: cover;
-  background-position: center;
-  opacity: 0.5;
 }
 
 .wrapper ul {
@@ -173,14 +78,153 @@ li {
   width: 90%;
 }
 
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 40rem;
+  width: 60rem;
+  background-color: black;
+  border: 1px solid red;
+
+  ul {
+    .item {
+      .inner {
+        width: 100%;
+        height: 100%;
+        cursor: pointer;
+        transition: transform 1s linear;
+      }
+    }
+
+    .init {
+      transform: translateY(100%);
+    }
+  }
+
+  ul li .image {
+    width: 100%;
+    height: 100%;
+    border-radius: 20px;
+    background-size: cover;
+    background-position: center;
+    opacity: 0.5;
+  }
+
+  ul {
+    li:nth-of-type(1) .inner {
+      transition-delay: 0s;
+    }
+
+    li:nth-of-type(2) .inner {
+      transition-delay: 0.1s;
+    }
+
+    li:nth-of-type(3) .inner {
+      transition-delay: 0.2s;
+    }
+
+    li:nth-of-type(4) .inner {
+      transition-delay: 0.3s;
+    }
+
+    li:nth-of-type(5) .inner {
+      transition-delay: 0.4s;
+    }
+
+    li:nth-of-type(6) .inner {
+      transition-delay: 0.5s;
+    }
+
+    /* image */
+    li:nth-of-type(1) .image {
+      background-image: url('/images/1.jpg');
+    }
+
+    li:nth-of-type(2) .image {
+      background-image: url('/images/1.jpg');
+    }
+
+    li:nth-of-type(3) .image {
+      background-image: url('/images/1.jpg');
+    }
+
+    li:nth-of-type(4) .image {
+      background-image: url('/images/1.jpg');
+    }
+
+    li:nth-of-type(5) .image {
+      background-image: url('/images/1.jpg');
+    }
+
+    li:nth-of-type(6) .image {
+      background-image: url('/images/1.jpg');
+    }
+  }
+}
+
+// 动画
+.wrapper.wrapperActive ul {
+  li.active {
+    width: 100%;
+  }
+
+  li:not(.active) {
+    height: 0;
+    width: 0;
+  }
+
+  li {
+    transition: height 1s linear, width 1s 0.5s linear;
+  }
+}
+
+/* 画 × */
+.wrapper ul li {
+  .dir .close {
+    z-index: 5;
+    position: absolute;
+    top: 50px;
+    width: 50px;
+    height: 50px;
+    /* opacity: 0; */
+  }
+
+  .dir .close {
+    position: absolute;
+    right: 50px;
+    width: 30px;
+    height: 30px;
+  }
+
+  .dir .close::before, .dir .close::after {
+    content: '';
+    display: block;
+    position: absolute;
+    width: 30px;
+    height: 4px;
+    background-color: white;
+  }
+
+  .dir .close::before {
+    transform: rotate(45deg);
+  }
+
+  .dir .close::after {
+    transform: rotate(-45deg);
+  }
+}
+
 .wrapper ul li {
   position: relative;
   flex: 0 1 auto;
   height: 100%;
   width: 100%;
-  border: 1px solid white;
+  border: 3px solid black;
   border-radius: 20px;
   background-color: #333;
+  /* 动画还原 */
+  transition: width 1s 0.5s linear, height 1s linear;
 }
 
 .wrapper ul li .title {
@@ -188,7 +232,7 @@ li {
   top: 50%;
   left: 50%;
   color: white;
-  /*居中字体*/
+  /* 居中字体 */
   transform: translate(-50%, -50%);
   font-size: 1.5rem;
   font-weight: bold;
@@ -196,44 +240,5 @@ li {
 
 .wrapper ul li:hover .image {
   opacity: 1;
-}
-
-.wrapper ul li:nth-of-type(1) .inner {
-  transition-delay: 0s;
-}
-.wrapper ul li:nth-of-type(2) .inner {
-  transition-delay: 0.1s;
-}
-.wrapper ul li:nth-of-type(3) .inner {
-  transition-delay: 0.2s;
-}
-.wrapper ul li:nth-of-type(4) .inner {
-  transition-delay: 0.3s;
-}
-.wrapper ul li:nth-of-type(5) .inner {
-  transition-delay: 0.4s;
-}
-.wrapper ul li:nth-of-type(6) .inner {
-  transition-delay: 0.5s;
-}
-
-/* image */
-.wrapper ul li:nth-of-type(1) .image {
-  background-image: url("/images/1.jpg");
-}
-.wrapper ul li:nth-of-type(2) .image {
-  background-image: url("/images/1.jpg");
-}
-.wrapper ul li:nth-of-type(3) .image {
-  background-image: url("/images/1.jpg");
-}
-.wrapper ul li:nth-of-type(4) .image {
-  background-image: url("/images/1.jpg");
-}
-.wrapper ul li:nth-of-type(5) .image {
-  background-image: url("/images/1.jpg");
-}
-.wrapper ul li:nth-of-type(6) .image {
-  background-image: url("/images/1.jpg");
 }
 </style>
--- docs/blog/project/转轴展开照片墙/note.md
+++ docs/blog/project/转轴展开照片墙/note.md
@@ -1,5 +1,18 @@
 # 转轴展开照片墙
 
-<!-- <OtherComponent/> -->
-
 <TurnImagesWalls/>
+
+---
+
+## 思路
+
+布局：先用 flex 居中，然后 space-between。
+
+自下而上的动画：直接用 transform: translateY(100%) 下放，然后依靠过渡来做。
+
+放大缩小动画：同样利用过渡来做，单击图片放大时，其他未被单击的图片变小。
+
+关闭按钮：直接 before、after 画一个出来即可，在添一个 hover 旋转的效果。
+
+
+过渡：transition 或 @keyframes 或 vue 的过渡语法，事实上我发现 vue 的过渡语法不仅更好写，也更易于维护，因为你知道所有的过渡就在那几个地方。
